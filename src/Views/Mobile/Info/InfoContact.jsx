559d02ae2e408dc75d70bc8222b8999e621cf999
iamabdullahmalik <malik.ab@hotmail.com>
2022-12-07 21:18:01 +0500

    Test The infoContact

--- src/Views/Mobile/Info/InfoContact.jsx
+++ src/Views/Mobile/Info/InfoContact.jsx
@@ -27,72 +27,78 @@ const ViewOnMapLink = styled.a`
 `;
 
 export const InfoContact = () => {
-  const [time, setTime] = useState(
-    new Date().toLocaleString('en-GB', {
-      timeZone: 'Europe/Brussels',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    })
-  );
+  return(
+    <></>
+  )
+}
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTime(
-        new Date().toLocaleString('en-GB', {
-          timeZone: 'Europe/Brussels',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
-        })
-      );
-    }, 1000);
+// export const InfoContact = () => {
+//   const [time, setTime] = useState(
+//     new Date().toLocaleString('en-GB', {
+//       timeZone: 'Europe/Brussels',
+//       hour: '2-digit',
+//       minute: '2-digit',
+//       second: '2-digit',
+//     })
+//   );
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-  return (
-    <InfoContactWrapper>
-      <BjornTextTopMobile to='/'>Info</BjornTextTopMobile>
+//   useEffect(() => {
+//     const timer = setInterval(() => {
+//       setTime(
+//         new Date().toLocaleString('en-GB', {
+//           timeZone: 'Europe/Brussels',
+//           hour: '2-digit',
+//           minute: '2-digit',
+//           second: '2-digit',
+//         })
+//       );
+//     }, 1000);
 
-      <InfoContactText>
-        Studio Bjorn Verlinde, <br /> Hugo Verriestlaan 5B - 8800 Roeselare,
-        Belgium
-        <br />
-        +32 (0)499 737 487
-        <br />
-        <ViewOnMapLink
-          href='https://goo.gl/maps/UWws26kdgo6k8ibD8'
-          target='_blank'>
-          {t('ViewOnMap')}
-        </ViewOnMapLink>
-      </InfoContactText>
-      <InfoContactText>
-        <ContactTitle>{t('GeneralInquiries')}</ContactTitle>
-        <br />
-        <a href='mailto:hello@bjornverlinde.studio'>
-          hello@bjornverlinde.studio
-        </a>
-      </InfoContactText>
-      <InfoContactText>
-        <ContactTitle>{t('JobsInternships')}</ContactTitle>
-        <br />
-        <a href='mailto:jobs@bjornverlinde.studio'>jobs@bjornverlinde.studio</a>
-      </InfoContactText>
-      <InfoContactText>
-        <ContactTitle>Instagram</ContactTitle>
-        <br />
-        <a href='https://www.instagram.com/bjornverlinde/' target='_blank'>
-          @bjornverlinde
-        </a>
-      </InfoContactText>
-      <InfoContactText>
-        <ContactTitle>{t('OfficeHours')}</ContactTitle>
-        <br />
-        {t('MondayFriday')}: 09:00-18:00 <br />
-        {t('BelgiumCurrentTime')}: {time}
-      </InfoContactText>
-    </InfoContactWrapper>
-  );
-};
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, []);
+//   return (
+//     <InfoContactWrapper>
+//       <BjornTextTopMobile to='/'>Info</BjornTextTopMobile>
+
+//       <InfoContactText>
+//         Studio Bjorn Verlinde, <br /> Hugo Verriestlaan 5B - 8800 Roeselare,
+//         Belgium
+//         <br />
+//         +32 (0)499 737 487
+//         <br />
+//         <ViewOnMapLink
+//           href='https://goo.gl/maps/UWws26kdgo6k8ibD8'
+//           target='_blank'>
+//           {t('ViewOnMap')}
+//         </ViewOnMapLink>
+//       </InfoContactText>
+//       <InfoContactText>
+//         <ContactTitle>{t('GeneralInquiries')}</ContactTitle>
+//         <br />
+//         <a href='mailto:hello@bjornverlinde.studio'>
+//           hello@bjornverlinde.studio
+//         </a>
+//       </InfoContactText>
+//       <InfoContactText>
+//         <ContactTitle>{t('JobsInternships')}</ContactTitle>
+//         <br />
+//         <a href='mailto:jobs@bjornverlinde.studio'>jobs@bjornverlinde.studio</a>
+//       </InfoContactText>
+//       <InfoContactText>
+//         <ContactTitle>Instagram</ContactTitle>
+//         <br />
+//         <a href='https://www.instagram.com/bjornverlinde/' target='_blank'>
+//           @bjornverlinde
+//         </a>
+//       </InfoContactText>
+//       <InfoContactText>
+//         <ContactTitle>{t('OfficeHours')}</ContactTitle>
+//         <br />
+//         {t('MondayFriday')}: 09:00-18:00 <br />
+//         {t('BelgiumCurrentTime')}: {time}
+//       </InfoContactText>
+//     </InfoContactWrapper>
+//   );
+// };
